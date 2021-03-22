5a839764ee0d19f5ac4f96704a292fe999f2c30f
Jwiggiff <friedman.josh03@gmail.com>
2021-03-22 14:53:13 -0400

    Update issueList.js

--- assets/js/issueList.js
+++ assets/js/issueList.js
@@ -1,10 +1,10 @@
 const list = document.querySelector(".issue-list");
 
-document.querySelector(".left").addEventListener("click", (e) => {
+document.querySelector(".left")?.addEventListener("click", (e) => {
   e.preventDefault();
   list.scrollBy(-700, 0);
 });
-document.querySelector(".right").addEventListener("click", (e) => {
+document.querySelector(".right")?.addEventListener("click", (e) => {
   e.preventDefault();
   list.scrollBy(700, 0);
 }); 
