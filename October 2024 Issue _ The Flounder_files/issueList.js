9816930f0c435e813468ac9e6327ff3f07c79718
FlipNand <sphinx-doles4x@icloud.com>
2025-10-10 18:15:13 -0400

    Added Fix to Oct 2024

---
+++ October 2024 Issue _ The Flounder_files/issueList.js
@@ -0,0 +1,10 @@
+const list = document.querySelector(".issue-list");
+
+document.querySelector(".left")?.addEventListener("click", (e) => {
+  e.preventDefault();
+  list.scrollBy(-700, 0);
+});
+document.querySelector(".right")?.addEventListener("click", (e) => {
+  e.preventDefault();
+  list.scrollBy(700, 0);
+}); 
